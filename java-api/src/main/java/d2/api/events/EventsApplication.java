08d97eac1f8433755b1c42ab418e96e2b1751bc1
jenniferoenning <oenningstangjennifer@gmail.com>
2021-07-06 20:26:01 -0300

    change

---
+++ java-api/src/main/java/d2/api/events/EventsApplication.java
@@ -0,0 +1,12 @@
+package d2.api.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApplication.class, args);
+	}
+}
